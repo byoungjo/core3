642a6cbbbe6bed2ba93d00243368a003a93fc82f
byoungjo <ishtar1313!!>
2022-03-14 00:35:12 +0900

    test

--- src/main/java/infPrj/core/OrderApp.java
+++ src/main/java/infPrj/core/OrderApp.java
@@ -7,15 +7,20 @@ import infPrj.core.member.MemberServiceImpl;
 import infPrj.core.order.Order;
 import infPrj.core.order.OrderService;
 import infPrj.core.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.util.Arrays;
 
 public class OrderApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.orderService();
 
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
