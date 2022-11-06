d086bccfb321478cee63fa95549d4d894eb8bc54
kimbj <kimbj123@gmail.com>
2022-11-06 23:18:39 +0900

    test 1

--- src/main/java/infPrj/core/AppConfig.java
+++ src/main/java/infPrj/core/AppConfig.java
@@ -15,6 +15,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
+    // test
     @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
